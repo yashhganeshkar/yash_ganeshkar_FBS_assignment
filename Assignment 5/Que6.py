184b3081133cf99773961c8e004cbd7152d72324
yashhganeshkar <119284023+yashhganeshkar@users.noreply.github.com>
2025-10-09 10:23:24 -0700

    Delete Assignment 5 directory

--- Assignment 5/Que6.py
+++
@@ -1,9 +0,0 @@
-
-# Write a program to print prime numbers between 1 to 100.
-
-for num in range(2,101):
-    for i in range(2,num):
-        if(num%i==0):
-            break
-    else:
-        print(num)
