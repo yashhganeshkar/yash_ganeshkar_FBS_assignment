5326fc362497ad0f9e6cfa65ec91ef3d8ac08618
yashhganeshkar <119284023+yashhganeshkar@users.noreply.github.com>
2025-10-09 10:25:03 -0700

    Delete Assignment 8 directory

--- Assignment 8/CircleArea.py
+++
@@ -1,8 +0,0 @@
-
-def areaCircle(radius):
-    area = 3.14*radius*radius
-    print(f"Area of Circle is ", area)
-
-radius = float(input("Enter Radius = "))
-
-areaCircle(radius)
--- Assignment 8/areaRectangle.py
+++
@@ -1,9 +0,0 @@
-
-def areaReactangle(length, breadth):
-    area = length * breadth
-    print("Area of Rectangle is ",area)
-
-length = float(input("Enter Length = "))
-breadth = float(input("Enter Breadth = "))
-
-areaReactangle(length, breadth)
